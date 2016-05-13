03ec838bc2ad5f03bf3f73a3730ad46cf2e40a70
Bohdan Khomtchouk <bohdankhomtchouk@UM-31HFG8WM.local>
2016-05-13 19:22:40 -0400

    add human

---
+++ R/human.r
@@ -0,0 +1,16 @@
+#' Homo sapiens (human) gene transfer format file data
+#'
+#' A dataset containing annotations of both coding and non-coding genes
+#' of gencode.v19.annotation.gtf
+#'
+#' @docType data
+#'
+#' @usage data(human)
+#'
+#' @format A data frame with 2619444 rows and 9 variables:
+#' \describe{
+#' \url{http://www.gencodegenes.org/gencodeformat.html}
+#' }
+#'
+#' @keywords datasets
+"human"
