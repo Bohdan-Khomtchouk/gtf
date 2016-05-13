ae31e748eee7b2dde5cae1a26d11537500333cf0
Bohdan Khomtchouk <bohdankhomtchouk@UM-31HFG8WM.local>
2016-05-13 19:11:13 -0400

    add mouse and rat

---
+++ R/rat.r
@@ -0,0 +1,16 @@
+#' Rattus norvegicus (rat) gene transfer format file data
+#'
+#' A dataset containing annotations of both coding and non-coding genes
+#' of Rattus_norvegicus.Rnor_6.0.82.gtf
+#'
+#' @docType data
+#'
+#' @usage data(rat)
+#'
+#' @format A data frame with 746468 rows and 9 variables:
+#' \describe{
+#' \url{http://www.gencodegenes.org/gencodeformat.html}
+#' }
+#'
+#' @keywords datasets
+"rat"
