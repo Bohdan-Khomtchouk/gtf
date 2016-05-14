255d4056380c35422b69b713bae6365b6d08db83
Bohdan Khomtchouk <bohdankhomtchouk@UM-31HFG8WM.local>
2016-05-13 20:16:35 -0400

    add frog

---
+++ R/frog.r
@@ -0,0 +1,16 @@
+#' Xenopus tropicalis (frog) gene transfer format file data
+#'
+#' A dataset containing annotations of both coding and non-coding genes
+#' of Xenopus_tropicalis.JGI_4.2.84.gtf
+#'
+#' @docType data
+#'
+#' @usage data(frog)
+#'
+#' @format A data frame with 581787 rows and 9 variables:
+#' \describe{
+#' \url{http://www.gencodegenes.org/gencodeformat.html}
+#' }
+#'
+#' @keywords datasets
+"frog"
